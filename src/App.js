95639d5a43c06ce5e0bc60aa31ddf0ba6f4d2cf6
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-03-27 12:39:13 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
 import PreviewCard from './components/PreviewCard';
@@ -12,12 +12,12 @@ function App() {
         <Title text="ozaki25's Link" />
         <div className="row">
           {links.map((link, i) => (
-            <>
+            <Fragment key={i}>
               <div className="col">
                 <PreviewCard {...link} />
               </div>
               {i % 2 === 1 && <div className="w-100 mb-3" />}
-            </>
+            </Fragment>
           ))}
         </div>
       </div>
