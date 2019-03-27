6d92420a5caaa03ae8c8bf90ceed40ab71c9d734
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-03-27 12:29:44 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -5,31 +5,24 @@ import PreviewCard from './components/PreviewCard';
 import links from './constants/links';
 
 function App() {
-  let rows = [];
-  links.forEach((link, i) => {
-    if (i % 2 === 0) rows.push([links[i], links[i + 1] || null]);
-  });
   return (
     <>
       <Navbar title="Portals Demo" />
       <div className="container">
         <Title text="ozaki25's Link" />
-        {rows.map(([link1, link2], i) => (
-          <div className="row mb-3" key={i}>
-            <div className="col-md-6">
-              <PreviewCard {...link1} />
-            </div>
-            {link2 && (
-              <div className="col-md-6">
-                <PreviewCard {...link2} />
+        <div className="row">
+          {links.map((link, i) => (
+            <>
+              <div className="col" key={i}>
+                <PreviewCard {...link} />
               </div>
-            )}
-          </div>
-        ))}
+              {i % 2 === 1 && <div className="w-100 mb-3" />}
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
 }
-// onMouseenterしたらモーダル的にportal出す
 
 export default App;
